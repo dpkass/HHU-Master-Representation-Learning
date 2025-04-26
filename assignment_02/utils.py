3c3551f9b2484c59f2178ab219cef2adc1378d47
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-04-26 16:20:26 +0200

    assignment 02: solution

--- assignment_02/utils.py
+++ assignment_02/utils.py
@@ -16,11 +16,11 @@ def imshow(img, i=0, mean=torch.tensor([0.0], dtype=torch.float32), std=torch.te
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
-@torch.no_grad()
-def prevalidate(model, val_loader, criterion, device):
+@torch.inference_mode()
+def prevalidate(model, val_dl, criterion, device):
     model.eval()
     loss_step = []
-    for inp_data, labels in val_loader:
+    for inp_data, labels in val_dl:
         inp_data, labels = inp_data.to(device), labels.to(device)
         outputs = model(inp_data)
         val_loss = criterion(outputs, labels)
@@ -30,11 +30,11 @@ def prevalidate(model, val_loader, criterion, device):
     return val_loss_epoch
 
 
-def pretrain_one_epoch(model, optimizer, train_loader, criterion,
+def pretrain_one_epoch(model, optimizer, train_dl, criterion,
                        device, dtype, scaler):
     model.train()
     loss_step = []
-    for data in tqdm(train_loader, leave=False, desc='Training'):
+    for data in tqdm(train_dl, leave=False, desc='Training'):
         optimizer.zero_grad(set_to_none=True)
         # Move the data to the GPU
         inp_data, labels = data
@@ -63,7 +63,7 @@ def save_model(model, path, epoch, optimizer, val_loss):
         'loss': val_loss,
         }, path)
 
-def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, device):
+def pretrain(model, optimizer, num_epochs, train_dl, val_dl, criterion, device):
     dict_log = {"train_loss": [], "val_loss": []}
     device = torch.device(device)
 
@@ -74,9 +74,9 @@ def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, 
     model = model.to(device)
     pbar = tqdm(range(num_epochs))
     for epoch in pbar:
-        loss_curr_epoch = pretrain_one_epoch(model, optimizer, train_loader, criterion,
+        loss_curr_epoch = pretrain_one_epoch(model, optimizer, train_dl, criterion,
                                              device=device, dtype=dtype, scaler=scaler)
-        val_loss = prevalidate(model, val_loader, criterion, device)
+        val_loss = prevalidate(model, val_dl, criterion, device)
 
         # Print epoch results to screen
         msg = f'Loss: Train {loss_curr_epoch:.3f} \t Val {val_loss:.3f}'
@@ -88,17 +88,18 @@ def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, 
         # Use this code to save the model with the best validation loss
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            save_model(model, f'best_model_min_val_loss.pth', epoch, optimizer, val_loss)
+            save_model(model, './models/best_pretrained_model_min_val_loss.pth', epoch, optimizer,
+                       val_loss)
     return dict_log
 
 
-@torch.no_grad()
-def validate(model, val_loader, device):
+@torch.inference_mode()
+def validate(model, val_dl, device):
     model.eval()
     criterion = nn.CrossEntropyLoss()
     correct, total = 0, 0
     loss_step = []
-    for data in val_loader:
+    for data in val_dl:
         inp_data, labels = data
         inp_data = inp_data.to(device)
         labels = labels.to(device)
@@ -115,7 +116,7 @@ def validate(model, val_loader, device):
 
 
 # Provided
-@torch.no_grad()
+@torch.inference_mode()
 def get_features(model, dataloader, device):
     model = model.to(device)
     feats, labs = [], []
@@ -132,12 +133,12 @@ def get_features(model, dataloader, device):
     return f,l
 
 
-def train_one_epoch(model, optimizer, train_loader, device):
+def train_one_epoch(model, optimizer, train_dl, device):
     model.train()
     criterion = nn.CrossEntropyLoss()
     loss_step = []
     correct, total = 0, 0
-    for data in train_loader:
+    for data in train_dl:
         # Move the data to the GPU
         inp_data, labels = data
         inp_data = inp_data.to(device)
@@ -158,18 +159,18 @@ def train_one_epoch(model, optimizer, train_loader, device):
     return loss_curr_epoch, train_acc
 
 
-def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
+def linear_eval(model, optimizer, num_epochs, train_dl, val_dl, device):
     best_val_loss = 1e8
     best_val_acc = 0
     model = model.to(device)
     dict_log = {"train_acc_epoch": [], "val_acc_epoch": [], "loss_epoch": [], "val_loss": []}
-    train_acc, _ = validate(model, train_loader, device)
-    val_acc, _ = validate(model, val_loader, device)
+    train_acc, _ = validate(model, train_dl, device)
+    val_acc, _ = validate(model, val_dl, device)
     print(f'Init Accuracy of the model: Train:{train_acc:.3f} \t Val:{val_acc:3f}')
     pbar = tqdm(range(num_epochs))
     for epoch in pbar:
-        loss_curr_epoch, train_acc = train_one_epoch(model, optimizer, train_loader, device)
-        val_acc, val_loss = validate(model, val_loader, device)
+        loss_curr_epoch, train_acc = train_one_epoch(model, optimizer, train_dl, device)
+        val_acc, val_loss = validate(model, val_dl, device)
 
         # Print epoch results to screen 
         msg = (f'Ep {epoch}/{num_epochs}: Accuracy : Train:{train_acc:.2f} \t Val:{val_acc:.2f} || Loss: Train {loss_curr_epoch:.3f} \t Val {val_loss:.3f}')
@@ -187,7 +188,7 @@ def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
                   'model_state_dict': model.state_dict(),
                   'optimizer_state_dict': optimizer.state_dict(),
                   'loss': val_loss,
-                  }, 'best_model_min_val_loss.pth')
+                  }, './models/best_linear_model_min_val_loss.pth')
         
         if val_acc > best_val_acc:
             best_val_acc = val_acc
@@ -196,7 +197,7 @@ def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
                   'model_state_dict': model.state_dict(),
                   'optimizer_state_dict': optimizer.state_dict(),
                   'loss': val_loss,
-                  }, 'best_model_max_val_acc.pth')
+                  }, './models/best_linear_model_max_val_acc.pth')
     return dict_log
 
 
